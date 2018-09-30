6351710263f6f3c1bd4279ed936b7a5211202611
toe_head2001 <toe_head2001@yahoo.com>
2018-09-29 21:38:24 -0600

    Update assembly info

--- RubberStamp/Properties/AssemblyInfo.cs
+++ RubberStamp/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("rubber stamp")]
 [assembly: AssemblyCompany("toe_head2001")]
 [assembly: AssemblyProduct("Rubber Stamp")]
-[assembly: AssemblyCopyright("Copyright © 2017 toe_head2001")]
+[assembly: AssemblyCopyright("Copyright © 2018 toe_head2001")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
