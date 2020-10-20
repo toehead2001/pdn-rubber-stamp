aa053d50dc17e16821782a80c86720ea25f00400
toehead2001 <toehead2001@users.noreply.github.com>
2020-10-19 19:54:29 -0600

    Renames

--- RubberStamp/RubberStamp.cs
+++ RubberStamp/RubberStamp.cs
@@ -21,12 +21,12 @@ namespace RubberStampEffect
     [PluginSupportInfo(typeof(PluginSupportInfo), DisplayName = "Rubber Stamp")]
     public class RubberStampEffectPlugin : PropertyBasedEffect
     {
-        private int Amount1 = 50; // [2,1000] Scale
-        private double Amount2 = 1.0; // [0,1] Roughness
-        private int Amount3 = 0; // [255] Reseed
-        private ColorBgra Amount4 = ColorBgra.Black; // Color
-        private int Amount5 = 85;
-        private bool Amount6 = false;
+        private int scale = 50; // [2,1000] Scale
+        private double roughness = 1.0; // [0,1] Roughness
+        private int reseed = 0; // [255] Reseed
+        private ColorBgra color = ColorBgra.Black; // Color
+        private int minOpacity = 85;
+        private bool customColor = false;
 
         private readonly CloudsEffect cloudsEffect = new CloudsEffect();
         private PropertyCollection cloudsProps;
@@ -43,12 +43,12 @@ namespace RubberStampEffect
 
         private enum PropertyNames
         {
-            Amount1,
-            Amount2,
-            Amount3,
-            Amount4,
-            Amount5,
-            Amount6
+            Scale,
+            Roughness,
+            Reseed,
+            Color,
+            MinimumOpacity,
+            UseCustomColor
         }
 
         [ThreadStatic]
@@ -63,17 +63,17 @@ namespace RubberStampEffect
 
             List<Property> props = new List<Property>
             {
-                new Int32Property(PropertyNames.Amount1, 50, 3, 100),
-                new DoubleProperty(PropertyNames.Amount2, 1.0, 0, 1.0),
-                new Int32Property(PropertyNames.Amount5, 85, byte.MinValue, byte.MaxValue),
-                new Int32Property(PropertyNames.Amount3, 0, 0, 255),
-                new BooleanProperty(PropertyNames.Amount6, false),
-                new Int32Property(PropertyNames.Amount4, ColorBgra.ToOpaqueInt32(PrimaryColor), 0, 0xffffff)
+                new Int32Property(PropertyNames.Scale, 50, 3, 100),
+                new DoubleProperty(PropertyNames.Roughness, 1.0, 0, 1.0),
+                new Int32Property(PropertyNames.MinimumOpacity, 85, byte.MinValue, byte.MaxValue),
+                new Int32Property(PropertyNames.Reseed, 0, 0, 255),
+                new BooleanProperty(PropertyNames.UseCustomColor, false),
+                new Int32Property(PropertyNames.Color, ColorBgra.ToOpaqueInt32(PrimaryColor), 0, 0xffffff)
             };
 
             List<PropertyCollectionRule> propRules = new List<PropertyCollectionRule>
             {
-                new ReadOnlyBoundToBooleanRule(PropertyNames.Amount4, PropertyNames.Amount6, true)
+                new ReadOnlyBoundToBooleanRule(PropertyNames.Color, PropertyNames.UseCustomColor, true)
             };
 
             return new PropertyCollection(props, propRules);
@@ -83,34 +83,34 @@ namespace RubberStampEffect
         {
             ControlInfo configUI = CreateDefaultConfigUI(props);
 
-            configUI.SetPropertyControlValue(PropertyNames.Amount1, ControlInfoPropertyNames.DisplayName, "Scale");
-            configUI.SetPropertyControlValue(PropertyNames.Amount2, ControlInfoPropertyNames.DisplayName, "Roughness");
-            configUI.SetPropertyControlValue(PropertyNames.Amount2, ControlInfoPropertyNames.SliderLargeChange, 0.25);
-            configUI.SetPropertyControlValue(PropertyNames.Amount2, ControlInfoPropertyNames.SliderSmallChange, 0.05);
-            configUI.SetPropertyControlValue(PropertyNames.Amount2, ControlInfoPropertyNames.UpDownIncrement, 0.01);
-            configUI.SetPropertyControlValue(PropertyNames.Amount2, ControlInfoPropertyNames.DecimalPlaces, 3);
-            configUI.SetPropertyControlValue(PropertyNames.Amount3, ControlInfoPropertyNames.DisplayName, string.Empty);
-            configUI.SetPropertyControlType(PropertyNames.Amount3, PropertyControlType.IncrementButton);
-            configUI.SetPropertyControlValue(PropertyNames.Amount3, ControlInfoPropertyNames.ButtonText, "Reseed");
-            configUI.SetPropertyControlValue(PropertyNames.Amount4, ControlInfoPropertyNames.DisplayName, string.Empty);
-            configUI.SetPropertyControlType(PropertyNames.Amount4, PropertyControlType.ColorWheel);
-            configUI.SetPropertyControlValue(PropertyNames.Amount5, ControlInfoPropertyNames.DisplayName, "Minimum Opacity");
-            configUI.SetPropertyControlValue(PropertyNames.Amount5, ControlInfoPropertyNames.ControlColors, new ColorBgra[] { ColorBgra.White, ColorBgra.Black });
-            configUI.SetPropertyControlValue(PropertyNames.Amount6, ControlInfoPropertyNames.DisplayName, "Color");
-            configUI.SetPropertyControlValue(PropertyNames.Amount6, ControlInfoPropertyNames.Description, "Use Custom Color");
+            configUI.SetPropertyControlValue(PropertyNames.Scale, ControlInfoPropertyNames.DisplayName, "Scale");
+            configUI.SetPropertyControlValue(PropertyNames.Roughness, ControlInfoPropertyNames.DisplayName, "Roughness");
+            configUI.SetPropertyControlValue(PropertyNames.Roughness, ControlInfoPropertyNames.SliderLargeChange, 0.25);
+            configUI.SetPropertyControlValue(PropertyNames.Roughness, ControlInfoPropertyNames.SliderSmallChange, 0.05);
+            configUI.SetPropertyControlValue(PropertyNames.Roughness, ControlInfoPropertyNames.UpDownIncrement, 0.01);
+            configUI.SetPropertyControlValue(PropertyNames.Roughness, ControlInfoPropertyNames.DecimalPlaces, 3);
+            configUI.SetPropertyControlValue(PropertyNames.Reseed, ControlInfoPropertyNames.DisplayName, string.Empty);
+            configUI.SetPropertyControlType(PropertyNames.Reseed, PropertyControlType.IncrementButton);
+            configUI.SetPropertyControlValue(PropertyNames.Reseed, ControlInfoPropertyNames.ButtonText, "Reseed");
+            configUI.SetPropertyControlValue(PropertyNames.Color, ControlInfoPropertyNames.DisplayName, string.Empty);
+            configUI.SetPropertyControlType(PropertyNames.Color, PropertyControlType.ColorWheel);
+            configUI.SetPropertyControlValue(PropertyNames.MinimumOpacity, ControlInfoPropertyNames.DisplayName, "Minimum Opacity");
+            configUI.SetPropertyControlValue(PropertyNames.MinimumOpacity, ControlInfoPropertyNames.ControlColors, new ColorBgra[] { ColorBgra.White, ColorBgra.Black });
+            configUI.SetPropertyControlValue(PropertyNames.UseCustomColor, ControlInfoPropertyNames.DisplayName, "Color");
+            configUI.SetPropertyControlValue(PropertyNames.UseCustomColor, ControlInfoPropertyNames.Description, "Use Custom Color");
 
             return configUI;
         }
 
         protected override void OnSetRenderInfo(PropertyBasedEffectConfigToken newToken, RenderArgs dstArgs, RenderArgs srcArgs)
         {
-            Amount1 = newToken.GetProperty<Int32Property>(PropertyNames.Amount1).Value;
-            Amount2 = newToken.GetProperty<DoubleProperty>(PropertyNames.Amount2).Value;
-            Amount3 = (byte)newToken.GetProperty<Int32Property>(PropertyNames.Amount3).Value;
-            randomSeed = Amount3;
-            Amount4 = ColorBgra.FromOpaqueInt32(newToken.GetProperty<Int32Property>(PropertyNames.Amount4).Value);
-            Amount5 = newToken.GetProperty<Int32Property>(PropertyNames.Amount5).Value;
-            Amount6 = newToken.GetProperty<BooleanProperty>(PropertyNames.Amount6).Value;
+            scale = newToken.GetProperty<Int32Property>(PropertyNames.Scale).Value;
+            roughness = newToken.GetProperty<DoubleProperty>(PropertyNames.Roughness).Value;
+            reseed = (byte)newToken.GetProperty<Int32Property>(PropertyNames.Reseed).Value;
+            randomSeed = reseed;
+            color = ColorBgra.FromOpaqueInt32(newToken.GetProperty<Int32Property>(PropertyNames.Color).Value);
+            minOpacity = newToken.GetProperty<Int32Property>(PropertyNames.MinimumOpacity).Value;
+            customColor = newToken.GetProperty<BooleanProperty>(PropertyNames.UseCustomColor).Value;
 
             if (emptySurface == null)
                 emptySurface = new Surface(srcArgs.Size);
@@ -120,10 +120,10 @@ namespace RubberStampEffect
             // Call the Render Clouds function
             cloudsProps = cloudsEffect.CreatePropertyCollection();
             PropertyBasedEffectConfigToken CloudsParameters = new PropertyBasedEffectConfigToken(cloudsProps);
-            CloudsParameters.SetPropertyValue(CloudsEffect.PropertyNames.Scale, Amount1);
-            CloudsParameters.SetPropertyValue(CloudsEffect.PropertyNames.Power, Amount2);
-            CloudsParameters.SetPropertyValue(CloudsEffect.PropertyNames.Seed, Amount3);
-            using (EffectEnvironmentParameters environParameters = new EffectEnvironmentParameters(ColorBgra.Black, Color.FromArgb(Amount5, Color.Black), 0, EnvironmentParameters.GetSelectionAsPdnRegion(), emptySurface))
+            CloudsParameters.SetPropertyValue(CloudsEffect.PropertyNames.Scale, scale);
+            CloudsParameters.SetPropertyValue(CloudsEffect.PropertyNames.Power, roughness);
+            CloudsParameters.SetPropertyValue(CloudsEffect.PropertyNames.Seed, reseed);
+            using (EffectEnvironmentParameters environParameters = new EffectEnvironmentParameters(ColorBgra.Black, Color.FromArgb(minOpacity, Color.Black), 0, EnvironmentParameters.GetSelectionAsPdnRegion(), emptySurface))
                 cloudsEffect.EnvironmentParameters = environParameters;
             cloudsEffect.SetRenderInfo(CloudsParameters, new RenderArgs(cloudSurface), new RenderArgs(emptySurface));
 
@@ -156,7 +156,7 @@ namespace RubberStampEffect
                 if (IsCancelRequested) return;
                 for (int x = rect.Left; x < rect.Right; x++)
                 {
-                    currentPixel = (Amount6) ? Amount4 : src[x, y];
+                    currentPixel = (customColor) ? color : src[x, y];
                     currentPixel.A = Int32Util.ClampToByte(cloudSurface[x, y].A + src[x, y].A - byte.MaxValue);
 
                     dst[x, y] = currentPixel;
